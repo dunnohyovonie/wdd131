e20599cec1fc6ff83449b2cf1a5dba48518d2f5f
dunnohyovonie <113483696+dunnohyovonie@users.noreply.github.com>
2025-06-06 18:44:34 +0000

    Update date.js

--- Scripts/date.js
+++ Scripts/date.js
@@ -1,2 +1,7 @@
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = document.lastModified;
+const lastModified = new Date(document.lastModified);
+const formatted = lastModified.toLocaleDateString("en-US", {
+  year: "numeric",
+  month: "long",
+  day: "numeric"
+});
+document.getElementById("lastModified").textContent = formatted;
